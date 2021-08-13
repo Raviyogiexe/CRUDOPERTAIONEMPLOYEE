5eb56e35a76edbe4e5808495d5d0eca9e0ab7421
DELL <DELL@RAVINDRA-VWIPL>
2021-08-13 19:31:04 +0530

    first commit

---
+++ EmployeeRegistrationpageUsingMVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeRegistrationpageUsingMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add_Employee_data(string txtemployee, string txtdob, string[] rdogender, string txtaddress, string ddlstate, string[] chkhobbye)
+        {
+
+            return PartialView("_Employeepart");
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
